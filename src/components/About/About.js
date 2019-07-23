3d4475341b5cff27b67da48e09b38088a8a17993
Mark Mode <markevan100@gmail.com>
2019-07-23 14:24:11 -0600

    Update about with text and photo.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import classes from './About.module.css';
+import image1 from './img7.jpg';
 
 const about = props => (
-    <div className={classes.About}> Hi, I'm a yoga teacher and blogger and I'm into gut biomes.</div>
+  <React.Fragment>
+    <div className={classes.About}>
+      Hi, I'm Johanna. I am a yoga teacher (200hr Yoga Alliance Certified), gut
+      science enthusiast, health investigator, sucker for health food marketing,
+      and sometimes writer. I like to spend my time considering ways to make sure
+      I have access to chocolate, how soon I can get to yoga class, stoopid
+      societal patterns, and one-liners for my non-existent Twitter.
+    </div>
+    <img src={image1} alt="Image" className={classes.Images}/>
+
+  </React.Fragment>
 );
 
 export default about;
